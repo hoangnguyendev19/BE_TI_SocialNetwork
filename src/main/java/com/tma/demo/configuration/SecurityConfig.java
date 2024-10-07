a20b90aac5800ae3c5625677fe15741d68b73c2b
tdnguyen <tdnguyen16102002@gmail.com>
2024-10-07 16:32:47 +0700

    update: login

--- src/main/java/com/tma/demo/configuration/SecurityConfig.java
+++ src/main/java/com/tma/demo/configuration/SecurityConfig.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,7 +25,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  * 04/10/2024        NGUYEN             create
  */
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     private final String[] PUBLIC_ENDPOINTS = {"/api/v1/auth/*"};
     private final AuthenticationProvider authenticationProvider;
