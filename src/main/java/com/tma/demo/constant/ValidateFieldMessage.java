35a8eac0db6dc4a03e8f4100b14e26ff605f6190
tdnguyen <tdnguyen16102002@gmail.com>
2024-10-14 20:45:31 +0700

    refactor

---
+++ src/main/java/com/tma/demo/constant/ValidateFieldMessage.java
@@ -0,0 +1,24 @@
+package com.tma.demo.constant;
+
+/**
+ * ValidateFieldMessage
+ * Version 1.0
+ * Date: 14/10/2024
+ * Copyright
+ * Modification Logs
+ * DATE          AUTHOR          DESCRIPTION
+ * ------------------------------------------------
+ * 14/10/2024        NGUYEN             create
+ */
+public class ValidateFieldMessage {
+    public static final String PASSWORD = "password-invalid";
+    public static final String EMAIL_ADDRESS = "email-invalid";
+    public static final String FIRST_NAME = "first-name-is-required";
+    public static final String LAST_NAME = "last-name-is-required";
+    public static final String DATE_OF_BIRTH = "date-of-birth-is-required";
+    public static final String PRESENT_ADDRESS = "presence-is-required";
+    public static final String PERMANENT_ADDRESS = "presence-is-required";
+    public static final String PHONE_NUMBER = "phone-number-is-required";
+    public static final String CITY = "city-is-required";
+    public static final String COUNTRY = "country-is-required";
+}
