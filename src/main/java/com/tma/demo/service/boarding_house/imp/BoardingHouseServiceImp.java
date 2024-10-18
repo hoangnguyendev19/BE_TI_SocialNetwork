fabf6221067d983d29601ed392182644ac67e3a2
tdnguyen <tdnguyen16102002@gmail.com>
2024-10-18 16:51:11 +0700

    features: Register boarding house

---
+++ src/main/java/com/tma/demo/service/boarding_house/imp/BoardingHouseServiceImp.java
@@ -0,0 +1,59 @@
+package com.tma.demo.service.boarding_house.imp;
+
+import com.tma.demo.common.ErrorCode;
+import com.tma.demo.dto.BoardingHouseDto;
+import com.tma.demo.entity.BoardingHouse;
+import com.tma.demo.exception.BaseException;
+import com.tma.demo.repository.BoardingHouseRepository;
+import com.tma.demo.service.boarding_house.BoardingHouseService;
+import com.tma.demo.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+/**
+ * BoardingHouseServiceImp
+ * Version 1.0
+ * Date: 18/10/2024
+ * Copyright
+ * Modification Logs
+ * DATE          AUTHOR          DESCRIPTION
+ * ------------------------------------------------
+ * 18/10/2024        NGUYEN             create
+ */
+@Service
+@RequiredArgsConstructor
+public class BoardingHouseServiceImp implements BoardingHouseService {
+    private final BoardingHouseRepository boardingHouseRepository;
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    @Override
+    @Transactional
+    public BoardingHouseDto register(BoardingHouseDto request) {
+        if(isBoardingHouseNameExists(request.getBoardingHouseName())){
+            throw new BaseException(ErrorCode.BOARDING_HOUSE_NAME_ALREADY_EXISTS);
+        }
+        BoardingHouse boardingHouse = BoardingHouse.builder()
+                .boardingHouseName(request.getBoardingHouseName())
+                .user(userService.getUserDetails())
+                .city(request.getCity())
+                .ward(request.getWard())
+                .isDelete(false)
+                .presentAddress(request.getPresentAddress())
+                .build();
+        boardingHouse = boardingHouseRepository.saveAndFlush(boardingHouse);
+        return modelMapper.map(boardingHouse, BoardingHouseDto.class);
+    }
+
+    private Boolean isBoardingHouseNameExists(String boardingHouseName) {
+        return boardingHouseRepository.isBoardingHouseNameExists(boardingHouseName);
+    }
+
+    public BoardingHouse getBoardingHouse(String id){
+        return boardingHouseRepository.findBoardingHouseById(UUID.fromString(id));
+    }
+}
