991591896160f0fc031b6d6ba594bde89209ac57
tdnguyen <tdnguyen16102002@gmail.com>
2024-10-22 15:50:05 +0700

    fix

--- src/main/java/com/tma/demo/service/user/imp/UserServiceImp.java
+++ src/main/java/com/tma/demo/service/user/imp/UserServiceImp.java
@@ -26,9 +26,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.sql.Date;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Base64;
 import java.util.Map;
 import java.util.UUID;
 
@@ -95,7 +92,7 @@ public class UserServiceImp implements UserService {
         String url;
         Map data;
         try {
-            data = cloudinaryService.upload(imageFile.getBytes(), folder, user.getId().toString());
+            data = cloudinaryService.upload(imageFile.getBytes(), MediaType.IMAGE, folder, user.getId().toString());
         } catch (IOException e) {
             throw new BaseException(ErrorCode.IMAGE_UPLOAD_FAILED);
         }
